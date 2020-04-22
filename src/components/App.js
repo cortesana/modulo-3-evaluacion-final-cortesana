549000698eaf664b4d66b2ca42806406db4111ac
cortesana <a.chdez2020@gmail.com>
2020-04-22 19:41:05 +0200

    Correct project structure

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import {fetchData} from '../services/dataAPI';
 import Header from './Header';
 import SearchBar from './SearchBar';
 import CharacterList from './CharacterList';
-import CharacterCard from './CharacterCard';
 import CharacterDetails from './CharacterDetails';
 
 class App extends React.Component {
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import CharacterCard from './CharacterCard';
 
 function CharacterList() {
 }

b9b70dfa381b7af7966a83b762681ca6bf536303
cortesana <a.chdez2020@gmail.com>
2020-04-24 13:20:43 +0200

    Add docs folder

---
+++ docs/precache-manifest.89e8309e0f6aba02b1ed1a15964211d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631c1947d3784416e963164d8f6cbea9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a5705b8e97a753cf719",
+    "url": "./static/css/main.b2939137.chunk.css"
+  },
+  {
+    "revision": "4c721532ea349d49ac40",
+    "url": "./static/js/2.0d653e81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.0d653e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a5705b8e97a753cf719",
+    "url": "./static/js/main.ca6b5e02.chunk.js"
+  },
+  {
+    "revision": "d6386c3a0f68325d8fba",
+    "url": "./static/js/runtime-main.e0fc0e3d.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
